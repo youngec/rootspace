c82e54e92b837e530a0b933d01fb99ae14a04dfe
Eleanore C. Young <email@bugmenot.com>
2016-09-16 17:56:11 +0200

    Fixed the issue with which attrs overwrote my custom __hash__ method. Also renamed the __setattr__ and __delattr__ methods as they were causing a conflict with attrs.

--- src/rootspace/entities.py
+++ src/rootspace/entities.py
@@ -7,6 +7,7 @@ import attr
 from attr.validators import instance_of
 
 from .worlds import World
+from .components import MachineState, NetworkState, FileSystem, TerminalDisplay
 
 
 @attr.s
@@ -14,8 +15,8 @@ class Entity(object):
     """
     An entity is a container with a unique identifier.
     """
-    _world = attr.ib(validator=instance_of(World))
-    _ident = attr.ib(default=attr.Factory(uuid.uuid4), validator=instance_of(uuid.UUID))
+    _world = attr.ib(validator=instance_of(World), hash=False)
+    _ident = attr.ib(validator=instance_of(uuid.UUID))
 
     @property
     def world(self):
@@ -36,16 +37,53 @@ class Entity(object):
         return self._ident
 
     @classmethod
-    def create(cls, world):
+    def create(cls, world, **kwargs):
         """
         Create an entity.
 
         :param world:
+        :param kwargs:
         :return:
         """
-        inst = cls(world)
-        world.entities.add(inst)
+        ident = uuid.uuid4()
+
+        if len(kwargs) > 0:
+            inst = cls(world, ident, **kwargs)
+        else:
+            inst = cls(world, ident)
+
+        world.add_entity(inst)
         return inst
+
+    def get_component(self, name):
+        """
+        Return a reference to an attached component.
+
+        :param name:
+        :return:
+        """
+        return self.__getattr__(name)
+
+    def set_component(self, name, value):
+        mro = inspect.getmro(value.__class__)
+        if type in mro:
+            stop = mro.index(type)
+        else:
+            stop = mro.index(object)
+
+        mro = mro[0:stop]
+        world_comp_types = self._world.component_types
+        for class_type in mro:
+            if class_type not in world_comp_types:
+                self._world.add_componenttype(class_type)
+            self._world.components[class_type][self] = value
+
+    def delete_component(self, name):
+        comp_type = self._world.component_types.get(name)
+        if comp_type is None:
+            raise AttributeError("{!r} has no attribute {!r}".format(self, name))
+
+        del self._world.components[comp_type][self]
 
     def delete(self):
         """
@@ -53,7 +91,7 @@ class Entity(object):
 
         :return:
         """
-        self._world.delete(self)
+        self._world.delete_entity(self)
 
     def __hash__(self):
         return hash(self._ident)
@@ -70,37 +108,3 @@ class Entity(object):
             raise AttributeError("{!r} has no attribute {!r}".format(self, item))
 
         return self._world.components[comp_type][self]
-
-    def __setattr__(self, key, value):
-        """
-        Set data within an attached component.
-
-        :param key:
-        :param value:
-        :return:
-        """
-        mro = inspect.getmro(value.__class__)
-        if type in mro:
-            stop = mro.index(type)
-        else:
-            stop = mro.index(object)
-
-        mro = mro[0:stop]
-        world_comp_types = self._world.component_types
-        for class_type in mro:
-            if class_type not in world_comp_types:
-                self._world.add_componenttype(class_type)
-            self._world.components[class_type][self] = value
-
-    def __delattr__(self, item):
-        """
-        Delete attached component data.
-
-        :param item:
-        :return:
-        """
-        comp_type = self._world.component_types.get(item)
-        if comp_type is None:
-            raise AttributeError("{!r} has no attribute {!r}".format(self, item))
-
-        del self._world.components[comp_type][self]
