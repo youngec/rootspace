4a52d95072d70370d40e88434910f113b39d8c17
Eleanore C. Young <email@bugmenot.com>
2016-10-13 21:40:34 +0200

    Updated the PYSDL2_DLL_PATH handling and copied it to the unit test configuration as well.

--- src/rootspace/tests/conftest.py
+++ src/rootspace/tests/conftest.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+
+import os
+import sys
+
+# Add the SDL2 library path (this is necessary for some linux versions)
+if sys.platform == "linux" and os.path.isdir("/run/current-system"):
+    os.environ["PYSDL2_DLL_PATH"] = "/run/current-system/sw/lib:{}".format(os.environ.get("PYSDL2_DLL_PATH", ""))
