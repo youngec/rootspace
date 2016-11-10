e7d268836ca59a34a80b21f6733b746e73d81763
Eleanore C. Young <email@bugmenot.com>
2016-11-10 21:23:47 +0100

    Added GLFW to the dependencies and removed PySDL2.

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             "colorlog",
             "attrs",
             "numpy",
-            "pysdl2",
+            "glfw",
             "xxhash"
         ],
         tests_require=[
