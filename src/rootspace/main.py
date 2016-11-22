e7376ada3a6ab88504b1de74e115ae1b0c008ea6
Eleanore Young <youngelliecaitlin@gmail.com>
2016-11-22 06:14:06 +0000

    Users can now choose to log output to a file instead.

--- src/rootspace/main.py
+++ src/rootspace/main.py
@@ -15,8 +15,10 @@ from .utilities import get_log_level, configure_logger
 @click.option("-v", "--verbose", count=True, help="Select the level of verbosity.")
 @click.option("--debug", is_flag=True, help="Enable debug mode.")
 @click.option("--profile", is_flag=True, help="Enable the profiler.")
+@click.option("--log-file", type=click.Path(dir_okay=False, writable=True), 
+              help="Output the log to the specified file")
 @click.version_option(get_versions()["version"])
-def main(verbose, debug, profile):
+def main(verbose, debug, profile, log_file):
     """
     Start a game using the rootspace game engine.
     Command line parameters take precedence over configuration values.
@@ -25,7 +27,7 @@ def main(verbose, debug, profile):
 
     # Configure the logging system.
     log_level = get_log_level(verbose, debug)
-    log = configure_logger(project_name, log_level, with_warnings=debug)
+    log = configure_logger(project_name, log_level, log_path=log_file, with_warnings=debug)
 
     # Create the engine instance
     loop = Loop(project_name, Context, debug)
