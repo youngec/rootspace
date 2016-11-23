34b46e415beb774cc6913b8b0003c25b3363ca58
Eleanore Young <youngelliecaitlin@gmail.com>
2016-11-23 08:45:55 +0000

    Fixed errors in the shader code

--- src/rootspace/resources/rootspace/shaders/simple_fragment.glsl
+++ src/rootspace/resources/rootspace/shaders/simple_fragment.glsl
@@ -1,11 +1,11 @@
 #version 330 core
 
-uniform sampler2D tex;
+uniform sampler2D active_tex;
 
 smooth in vec2 frag_tex_uv;
 
 out vec4 fragColor;
 
 void main() {
-    fragColor = texture(tex, frag_tex_uv);
+    fragColor = texture(active_tex, frag_tex_uv);
 }
