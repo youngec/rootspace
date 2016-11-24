820456d2355c22bfcb3eb609fd27a5b0c0bf71db
Eleanore Young <youngelliecaitlin@gmail.com>
2016-11-24 16:15:15 +0000

    Wrote my own Quaternion implementation and got rid of the package

--- src/rootspace/opengl_math.py
+++ src/rootspace/opengl_math.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import collections
 import math
 import numpy
-import quaternion
+
+import attr
+from attr.validators import instance_of
 
 
 def identity():
     return numpy.eye(4)
-
-
-def orientation(quat):
-    ori = numpy.eye(4)
-    ori[:3, :3] = quaternion.as_rotation_matrix(quat)
-    return ori
 
 
 def translation(vect):
@@ -49,3 +46,58 @@ def perspective(field_of_view, viewport_ratio, near, far):
         (0, 0, -(f + n) / (f - n), -1),
         (0, 0, -2 * (f * n) / (f - n), 1)
     ))
+
+
+@attr.s
+class Quaternion(object):
+    r = attr.ib(default=1.0, validator=instance_of(float), convert=float)
+    i = attr.ib(default=0.0, validator=instance_of(float), convert=float)
+    j = attr.ib(default=0.0, validator=instance_of(float), convert=float)
+    k = attr.ib(default=0.0, validator=instance_of(float), convert=float)
+
+    @property
+    def T(self):
+        return Quaternion(self.r, -self.i, -self.j, -self.k)
+
+    @property
+    def matrix3(self):
+        r = self.r
+        i = self.i
+        j = self.j
+        k = self.k
+
+        return numpy.array((
+            (1 - 2 * (j**2 + k**2), 2 * (i*j - k*r), 2 * (i*k + j*r)),
+            (2 * (i*j + k*r), 1 - 2 * (i**2 + k**2), 2 * (j*k - i*r)),
+            (2 * (i*k - j*r), 2 * (j*k + i*r), 1 - 2 * (i**2 + j**2))
+        ))
+
+    @property
+    def matrix4(self):
+        q = numpy.eye(4)
+        q[:3, :3] = self.matrix3
+        return q
+
+    def __mul__(self, other):
+        return Quaternion(self.r * other.r, self.i * other.i, self.j * other.j, self.k * other.k)
+
+    def __matmul__(self, other):
+        return Quaternion(
+            self.r * other.r - self.i * other.i - self.j * other.j - self.k * other.k,
+            self.r * other.i + self.i * other.r + self.j * other.k - self.k * other.j,
+            self.r * other.j - self.i * other.k + self.j * other.r + self.k * other.i,
+            self.r * other.k + self.i * other.j - self.j * other.i + self.k * other.r
+        )
+
+    @classmethod
+    def from_axis(cls, axis, angle):
+        axis /= numpy.linalg.norm(axis)
+        angle %= 2 * math.pi
+
+        sin = math.sin(angle / 2)
+        return cls(
+            math.cos(angle / 2),
+            axis[0] * sin, 
+            axis[1] * sin, 
+            axis[2] * sin
+        )
