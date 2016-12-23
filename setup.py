f5e76cca9515e216be21db6731b5d5503a0cc748
Eleanore Young <youngelliecaitlin@gmail.com>
2016-12-23 16:04:36 +0100

    Added a few additional dependencies for playing around

--- setup.py
+++ setup.py
@@ -88,7 +88,10 @@ if __name__ == "__main__":
             "numpy",
             "glfw",
             "pyopengl",
-            "xxhash"
+            "xxhash",
+            "pillow",
+            "pyparsing",
+            "regex"
         ],
         tests_require=[
             "pytest",
