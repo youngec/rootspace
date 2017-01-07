de029da86b9c8a40b767d0b4236f814dcd6cb491
Eleanore Young <youngelliecaitlin@gmail.com>
2017-01-07 12:37:06 +0100

    Having a lot of trouble with extending the parser.

--- src/rootspace/parsers.py
+++ src/rootspace/parsers.py
@@ -3,8 +3,42 @@
 
 """Implements parsers for resource files."""
 
+import typing
+import array
 import pyparsing as pp
 import attr
+from attr.validators import instance_of
+
+
+@attr.s
+class Mesh(object):
+    vertices = attr.ib()
+    faces = attr.ib()
+    edges = attr.ib()
+    other = attr.ib(default=attr.Factory(dict), validator=instance_of(dict))
+
+    def __getattr__(self, item):
+        if item in self.other:
+            return self.other[item]
+        else:
+            raise AttributeError("No attribute named '{}'.".format(item))
+
+
+@attr.s
+class Material(object):
+    pass
+
+
+@attr.s
+class Texture(object):
+    pass
+
+
+@attr.s
+class Model(object):
+    meshes = attr.ib(validator=instance_of(typing.List[Mesh]))
+    materials = attr.ib(validator=instance_of(typing.List[Material]))
+    textures = attr.ib(validator=instance_of(typing.List[Texture]))
 
 
 @attr.s
@@ -26,81 +60,130 @@ class PlyParser(object):
     body            ::= statement+
     statement       ::= NUMBER+
     """
-    grammar = attr.ib()
+    grammar = attr.ib(validator=instance_of(pp.ParserElement))
 
     @classmethod
     def create(cls):
+        """
+        Create a Stanford polygon file parser (PLY).
+
+        :return:
+        """
+        # Define the base patterns for parsing
         number = pp.pyparsing_common.number()
         identifier = pp.pyparsing_common.identifier()
-        lit = {l: pp.CaselessKeyword(l) for l in (
-            "ascii", "binary_little_endian", "binary_big_endian", "vertex", "face", "edge", "char",
-            "uchar", "short", "ushort", "int", "uint", "float", "double", "format", "comment",
-            "element", "property", "list", "ply", "end_header"
-        )}
 
-        format_type = lit["ascii"] | lit["binary_little_endian"] | lit["binary_big_endian"]
-        element_type = lit["vertex"] | lit["face"] | lit["edge"] | identifier
-        data_type = lit["char"] | lit["uchar"] | lit["short"] | lit["ushort"] | lit["int"] | lit["uint"] | \
-            lit["float"] | lit["double"]
+        # Define the suppressed keywords
+        start_keyword = pp.Suppress(pp.CaselessKeyword("ply"))
+        stop_keyword = pp.Suppress(pp.CaselessKeyword("end_header"))
+        comment_keyword = pp.Suppress(pp.CaselessKeyword("comment"))
+        format_keyword = pp.Suppress(pp.CaselessKeyword("format"))
+        element_keyword = pp.Suppress(pp.CaselessKeyword("element"))
+        property_keyword = pp.Suppress(pp.CaselessKeyword("property"))
+        list_keyword = pp.Suppress(pp.CaselessKeyword("list"))
 
-        comment_decl = lit["comment"] + pp.restOfLine
+        # Define the necessary keywords
+        def keyword_or(*keywords):
+            if isinstance(keywords[0], dict):
+                return pp.Or(pp.CaselessKeyword(l).addParseAction(pp.replaceWith(d)) for l, d in keywords[0].items())
+            else:
+                return pp.Or(pp.CaselessKeyword(literal) for literal in keywords)
 
-        format_decl = pp.Group(
-            pp.Suppress(lit["format"]) +
+        # TODO: Get binary PLY files to parse
+        # format_types = ("ascii", "binary_little_endian", "binary_big_endian")
+        format_type = keyword_or("ascii")
+        element_type = keyword_or("vertex", "face", "edge") | identifier
+        property_type = keyword_or({
+            "char": "b",
+            "uchar": "B",
+            "short": "h",
+            "ushort": "H",
+            "int": "i",
+            "uint": "I",
+            "float": "f",
+            "double": "d",
+            "int8": "b",
+            "uint8": "B",
+            "int16": "h",
+            "uint16": "H",
+            "int32": "i",
+            "uint32": "I",
+            "float32": "f",
+            "float64": "d"
+        })
+        property_position = keyword_or("x", "y", "z")
+        property_color = keyword_or("r", "g", "b", "a", "red", "green", "blue", "alpha")
+        property_ambient_color = keyword_or("ambient_red", "ambient_green", "ambient_blue", "ambient_alpha")
+        property_diffuse_color = keyword_or("diffuse_red", "diffuse_green", "diffuse_blue", "diffuse_alpha")
+        property_specular_color = keyword_or("specular_red", "specular_green", "specular_blue", "specular_alpha")
+        property_texture = keyword_or("s", "t", "u", "v", "tx", "ty")
+        property_normal = keyword_or("nx", "ny", "nz")
+        property_vertex_index = keyword_or("vertex_index", "vertex_indices")
+        property_material_index = keyword_or("material_index", "material_indices")
+        property_specular_power = keyword_or("specular_power")
+        property_opacity = keyword_or("opacity")
+
+        # Define the grammar of statements
+        comment_expr = pp.Group(
+            comment_keyword +
+            pp.restOfLine
+        )("comment")
+
+        format_expr = pp.Group(
+            format_keyword +
             format_type("file_type") +
             number("version")
         )("format")
 
-        property_simple = pp.Group(
-            pp.Suppress(lit["property"]) +
-            data_type("data_type") +
+        property_simple_expr = pp.Group(
+            property_keyword +
+            property_type("data_type") +
             identifier("name")
-        )("simple")
+        )
 
-        property_list = pp.Group(
-            pp.Suppress(lit["property"]) + pp.Suppress(lit["list"]) +
-            data_type("index_type") +
-            data_type("data_type") +
+        property_list_expr = pp.Group(
+            property_keyword + list_keyword +
+            property_type("index_type") +
+            property_type("data_type") +
             identifier("name")
-        )("list")
+        )
 
-        element_decl = pp.Group(
-            pp.Suppress(lit["element"]) +
-            element_type("name") +
-            number("count") +
+        element_expr = element_keyword + element_type("type") + number("count")
+
+        # Define the grammar of statement groups
+        element_group = pp.Group(
+            element_expr +
             pp.Group(
-                pp.OneOrMore(property_simple) | property_list
+                pp.OneOrMore(property_simple_expr) | property_list_expr
             )("properties")
         )
 
         declarations = pp.Group(
-            format_decl +
+            format_expr +
             pp.Group(
-                pp.OneOrMore(element_decl)
+                pp.OneOrMore(element_group)
             )("elements")
         )("declarations")
 
-        header_start = pp.Suppress(lit["ply"])
-
-        header_stop = pp.Suppress(lit["end_header"])
-
-        header = header_start + declarations + header_stop
+        header = start_keyword + declarations + stop_keyword
 
         body = pp.Forward()("data")
 
-        ply_grammar = (header + body).ignore(comment_decl)
+        ply_grammar = (header + body).ignore(comment_expr)
 
+        # Define the grammar of the body based on the header data
         def construct_body_expr(source, location, tokens):
             body_expr = list()
             for el_decl in tokens.declarations.elements:
-                if el_decl.properties[0].getName() == "list":
+                props = el_decl.properties
+                if len(props) == 1 and "index_type" in props[0]:
                     element = pp.countedArray(number)
                 else:
                     element = pp.Group(
-                        pp.And(number(p.name) for p in el_decl.properties)
+                        pp.And(number(p.name) for p in props)
                     )
 
-                body_expr.append(pp.Group(element * el_decl.count)(el_decl.name))
+                body_expr.append(pp.Group(element * el_decl.count)(el_decl.type))
 
             body << pp.And(body_expr)
 
@@ -110,19 +193,28 @@ class PlyParser(object):
 
     def tokenize(self, data):
         """
-        Parse the supplied data into an abstract syntax tree.
+        Parse the supplied data into a token tree.
 
         :param data:
         :return:
         """
         return self.grammar.parseString(data)
 
+    def parse(self, data):
+        """
+        Parse the supplied data into a model.
+
+        :param data:
+        :return:
+        """
+        tokens = self.tokenize(data)
+
 
 def main():
     data = """ply
 format ascii 1.0
-comment author: Greg Turk
-comment object: another cube
+comment author: Greg Turk and Eleanore Young
+comment object: a cube
 element vertex 8
 property float x
 property float y
@@ -130,6 +222,9 @@ property float z
 property uchar red
 property uchar green
 property uchar blue
+property uchar alpha
+property float s
+property float t
 element face 7
 property list uchar int vertex_index
 element edge 5
@@ -138,15 +233,16 @@ property int vertex2
 property uchar red
 property uchar green
 property uchar blue
+property uchar alpha
 end_header
-0 0 0 255 0 0
-0 0 1 255 0 0
-0 1 1 255 0 0
-0 1 0 255 0 0
-1 0 0 0 0 255
-1 0 1 0 0 255
-1 1 1 0 0 255
-1 1 0 0 0 255
+0 0 0 255 0 0 255 0 0
+0 0 1 255 0 0 255 0 1
+0 1 1 255 0 0 255 0 0
+0 1 0 255 0 0 255 0 1
+1 0 0 0 0 255 255 1 0
+1 0 1 0 0 255 255 1 1
+1 1 1 0 0 255 255 0 0
+1 1 0 0 0 255 255 0 0
 3 0 1 2
 3 0 2 3
 4 7 6 5 4
@@ -154,18 +250,16 @@ end_header
 4 1 5 6 2
 4 2 6 7 3
 4 3 7 4 0
-0 1 255 255 255
-1 2 255 255 255
-2 3 255 255 255
-3 0 255 255 255
-2 0 0 0 0"""
+0 1 255 255 255 255
+1 2 255 255 255 255
+2 3 255 255 255 255
+3 0 255 255 255 255
+2 0 0 0 0 255"""
 
     try:
         parser = PlyParser.create()
-        tokens = parser.tokenize(data)
-        #tokens.pprint()
-        print(tokens.dump())
-        #print(len(tokens.body))
+        model = parser.tokenize(data)
+        print(model)
     except pp.ParseException as e:
         print(e.line)
         print(" " * (e.column - 1) + "^")
