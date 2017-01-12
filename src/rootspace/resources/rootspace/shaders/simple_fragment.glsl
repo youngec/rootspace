2963dcadfbcbc21ed3fbf1add6505f6eb3d9ba50
Eleanore Young <youngelliecaitlin@gmail.com>
2017-01-12 11:47:52 +0100

    Modified the simple shaders to also calculate in a color attribute.

--- src/rootspace/resources/rootspace/shaders/simple_fragment.glsl
+++ src/rootspace/resources/rootspace/shaders/simple_fragment.glsl
@@ -3,9 +3,11 @@
 uniform sampler2D active_tex;
 
 smooth in vec2 frag_tex_uv;
+smooth in vec3 frag_color_rgb;
 
 out vec4 fragColor;
 
 void main() {
-    fragColor = texture(active_tex, frag_tex_uv);
+    // fragColor = texture(active_tex, frag_tex_uv);
+    fragColor = texture(active_tex, frag_tex_uv) * vec4(frag_color_rgb, 1.0);
 }
--- src/rootspace/resources/rootspace/shaders/simple_vertex.glsl
+++ src/rootspace/resources/rootspace/shaders/simple_vertex.glsl
@@ -2,12 +2,15 @@
 
 layout(location = 0) in vec3 vert_xyz;
 layout(location = 1) in vec2 tex_uv;
+layout(location = 2) in vec3 color_rgb;
 
 uniform mat4 mvp_matrix;
 
 smooth out vec2 frag_tex_uv;
+smooth out vec3 frag_color_rgb;
 
 void main() {
     gl_Position = mvp_matrix * vec4(vert_xyz, 1.0);
+    frag_color_rgb = color_rgb;
     frag_tex_uv = tex_uv;
 }
