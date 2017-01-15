7524bba8ec1e0a6c3b3974c1b43c4a72e39f4bd0
Eleanore Young <youngelliecaitlin@gmail.com>
2017-01-15 16:50:51 +0100

    Refactored the events from namedtuples to attrs classes.

--- src/rootspace/events.py
+++ src/rootspace/events.py
@@ -1,13 +1,67 @@
 # -*- coding: utf-8 -*-
 
-import collections
+"""The following classes encapsulate GLFW event concepts."""
+
+import attr
 
 
-KeyEvent = collections.namedtuple("KeyEvent", ("window", "key", "scancode", "action", "mods"))
-CharEvent = collections.namedtuple("CharEvent", ("window", "codepoint"))
-CursorEvent = collections.namedtuple("CursorEvent", ("window", "xpos", "ypos"))
-CursorEnterEvent = collections.namedtuple("CursorEnterEvent", ("window", "entered"))
-MouseButtonEvent = collections.namedtuple("MouseButtonEvent", ("window", "button", "action", "mods"))
-ScrollEvent = collections.namedtuple("ScrollEvent", ("window", "xoffset", "yoffset"))
+@attr.s
+class KeyEvent(object):
+    window = attr.ib()
+    key = attr.ib()
+    scancode = attr.ib()
+    action = attr.ib()
+    mods = attr.ib()
 
-KeyMap = collections.namedtuple("KeyMap", ("left", "right", "up", "down", "forward", "backward"))
+
+@attr.s
+class CharEvent(object):
+    window = attr.ib()
+    codepoint = attr.ib()
+
+
+@attr.s
+class CursorEvent(object):
+    window = attr.ib()
+    xpos = attr.ib()
+    ypos = attr.ib()
+
+
+@attr.s
+class CursorEnterEvent(object):
+    window = attr.ib()
+    entered = attr.ib()
+
+
+@attr.s
+class MouseButtonEvent(object):
+    window = attr.ib()
+    button = attr.ib()
+    action = attr.ib()
+    mods = attr.ib()
+
+
+@attr.s
+class ScrollEvent(object):
+    window = attr.ib()
+    xoffset = attr.ib()
+    yoffset = attr.ib()
+
+
+@attr.s
+class KeyMap(object):
+    left = attr.ib()
+    right = attr.ib()
+    up = attr.ib()
+    down = attr.ib()
+    forward = attr.ib()
+    backward = attr.ib()
+
+    def __iter__(self):
+        """
+        Allow iteration over the KeyMap member variables.
+
+        :return:
+        """
+        for member in attr.astuple(self):
+            yield member
