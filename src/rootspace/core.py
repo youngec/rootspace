10a142dceccce0e0690b57df7e0ece0c29734ec1
Eleanore Young <youngelliecaitlin@gmail.com>
2017-01-18 12:35:11 +0100

    Fully implemented the new Context data model (with ContextData and KeyMap).

--- src/rootspace/core.py
+++ src/rootspace/core.py
@@ -8,7 +8,6 @@ import collections
 import contextlib
 import json
 import logging
-import math
 import pathlib
 import shutil
 import uuid
@@ -22,12 +21,13 @@ import numpy
 from attr.validators import instance_of
 from PIL import Image
 
-from .events import KeyEvent, CharEvent, CursorEvent, KeyMap
+from .events import KeyEvent, CharEvent, CursorEvent
 from .exceptions import GLFWError, FixmeWarning
 from .components import Transform, CameraData
 from .utilities import subclass_of
 from .wrappers import Model, Shader
 from .model_parser import PlyParser
+from .data_abstractions import KeyMap, ContextData
 
 
 @attr.s(hash=False)
@@ -208,7 +208,7 @@ class CameraControlSystem(EventSystem):
     event_types = (KeyEvent, CursorEvent)
 
     def dispatch(self, event, world, components):
-        key_map = world.ctx.data.key_map
+        key_map = world.ctx.key_map
         dt = world.ctx.data.delta_time
 
         if isinstance(event, KeyEvent) and event.key in key_map and event.mods == 0:
@@ -632,65 +632,38 @@ class Context(object):
     The Context is used by the Engine to set the main loop parameters,
     the systems, entities, resources, states, etc.
     """
-    Data = collections.namedtuple("Data", (
-        "delta_time", "max_frame_duration", "epsilon", "window_title", "window_shape",
-        "window_hints", "swap_interval", "clear_color", "clear_bits", "field_of_view",
-        "near_plane", "far_plane", "key_map", "extra"
-    ))
-
-    default_ctx = Data(
-        delta_time=0.01,
-        max_frame_duration=0.25,
-        epsilon=1e-5,
-        window_title="Untitled",
-        window_shape=(1024, 768),
-        window_hints={
-            glfw.CONTEXT_VERSION_MAJOR: 3,
-            glfw.CONTEXT_VERSION_MINOR: 3,
-            glfw.OPENGL_FORWARD_COMPAT: True,
-            glfw.OPENGL_PROFILE: glfw.OPENGL_CORE_PROFILE
-        },
-        swap_interval=1,
-        clear_color=(0, 0, 0, 1),
-        clear_bits=(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT),
-        field_of_view=math.pi,
-        near_plane=0.1,
-        far_plane=100.0,
-        key_map=KeyMap(glfw.KEY_A, glfw.KEY_D, glfw.KEY_Z, glfw.KEY_X, glfw.KEY_W, glfw.KEY_S),
-        extra=None
-    )
     default_config_dir = ".config"
     default_resources_dir = "resources"
     default_config_file = "config.json"
+    default_keymap_file = "keymap.json"
 
     _name = attr.ib(validator=instance_of(str))
     _resources_root = attr.ib(validator=instance_of(pathlib.Path), repr=False)
     _states_root = attr.ib(validator=instance_of(pathlib.Path), repr=False)
-    _data = attr.ib(validator=instance_of(Data), repr=False)
-    _window = attr.ib()
-    _world = attr.ib(validator=instance_of((type(None), World)))
+    _data = attr.ib(validator=instance_of(ContextData))
+    _key_map = attr.ib(validator=instance_of(KeyMap))
     _debug = attr.ib(validator=instance_of(bool))
     _log = attr.ib(validator=instance_of(logging.Logger), repr=False)
-    _ctx_exit = attr.ib(validator=instance_of((type(None), contextlib.ExitStack)), repr=False)
+    _window = attr.ib(default=None, repr=False)
+    _world = attr.ib(default=None, validator=instance_of((type(None), World)))
+    _ctx_exit = attr.ib(default=None, validator=instance_of((type(None), contextlib.ExitStack)), repr=False)
 
     @classmethod
-    def create(cls, name, user_home, engine_location, debug=False):
+    def _ensure_config(cls, resources_path, states_path, force=False):
         """
-        Create a new project instance.
+        Initialize the persistent configuration of the context. If force is True,
+        overwrite any existing configuration in the current user directory.
 
-        :param name:
-        :param user_home:
-        :param engine_location:
-        :param debug:
+        :param resources_path:
+        :param states_path:
+        :param force:
         :return:
         """
-        # Specify the configuration directory and the resources directory
-        resources_path = engine_location / cls.default_resources_dir / name
-        states_path = user_home / cls.default_config_dir / name
-
         # Specify the configuration file paths
         config_default = resources_path / cls.default_config_file
+        keymap_default = resources_path / cls.default_keymap_file
         config_user = states_path / cls.default_config_file
+        keymap_user = states_path / cls.default_keymap_file
 
         # Ensure that both directories (resources and states) are present
         if not resources_path.exists():
@@ -698,21 +671,51 @@ class Context(object):
         elif not resources_path.is_dir():
             raise NotADirectoryError(resources_path)
 
+        # Create the user config directory, unless it exists
         if not states_path.exists():
             states_path.mkdir(parents=True)
 
         # Copy the default configuration to the user-specific directory
-        if not config_user.exists():
+        if not config_user.exists() or force:
             shutil.copyfile(str(config_default), str(config_user))
+
+        # Copy the default key map to the user-specific directory
+        if not keymap_user.exists() or force:
+            shutil.copyfile(str(keymap_default), str(keymap_user))
+
+        return config_user, keymap_user
+
+    @classmethod
+    def create(cls, name, user_home, engine_location, initialize=False, debug=False):
+        """
+        Create a new project instance.
+
+        :param name:
+        :param user_home:
+        :param engine_location:
+        :param initialize:
+        :param debug:
+        :return:
+        """
+        # Specify the configuration directory and the resources directory
+        resources_path = engine_location / cls.default_resources_dir / name
+        states_path = user_home / cls.default_config_dir / name
+
+        # Initialize the persistent configuration
+        config_user, keymap_user = cls._ensure_config(resources_path, states_path, initialize)
 
         # Load the configuration
         with config_user.open(mode="r") as f:
-            ctx = cls.default_ctx._replace(**json.load(f))
+            data = ContextData.from_dict(**json.load(f))
+
+        # Load the keymap
+        with keymap_user.open(mode="r") as f:
+            key_map = KeyMap.from_dict(**json.load(f))
 
         # Create the logger
         log = logging.getLogger("{}.{}".format(__name__, cls.__name__))
 
-        return cls(name, resources_path, states_path, ctx, None, None, debug, log, None)
+        return cls(name, resources_path, states_path, data, key_map, debug, log)
 
     @property
     def resources(self):
@@ -740,6 +743,15 @@ class Context(object):
         :return:
         """
         return self._data
+
+    @property
+    def key_map(self):
+        """
+        Return the key map.
+
+        :return:
+        """
+        return self._key_map
 
     @property
     def window(self):
@@ -949,6 +961,7 @@ class Loop(object):
     """
     _name = attr.ib(validator=instance_of(str))
     _ctx = attr.ib(default=Context, validator=subclass_of(Context), repr=False)
+    _initialize = attr.ib(default=False, validator=instance_of(bool))
     _debug = attr.ib(default=False, validator=instance_of(bool))
     _log = attr.ib(default=logging.getLogger(__name__), validator=instance_of(logging.Logger), repr=False)
 
@@ -964,7 +977,7 @@ class Loop(object):
         self._log.debug("The user home is at '{}'.".format(user_home))
         self._log.debug("The engine is located at '{}'.".format(engine_location))
 
-        with self._ctx.create(self._name, user_home, engine_location, self._debug) as ctx:
+        with self._ctx.create(self._name, user_home, engine_location, self._initialize, self._debug) as ctx:
             self._log.debug("Entered context {}.".format(ctx))
             self._loop(ctx)
 
---
+++ src/rootspace/data_abstractions.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+"""
+Define data abstractions for various concepts.
+"""
+
+import attr
+import enum
+from attr.validators import instance_of
+from .utilities import iterable_of
+import glfw
+import math
+from OpenGL.GL import GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
+
+
+@attr.s
+class ContextData(object):
+    """
+    Contains simple Context data.
+    """
+    delta_time = attr.ib(default=0.01, validator=instance_of(float))
+    max_frame_duration = attr.ib(default=0.25, validator=instance_of(float))
+    epsilon = attr.ib(default=1e-5, validator=instance_of(float))
+    window_title = attr.ib(default="Untitled", validator=instance_of(str))
+    window_shape = attr.ib(default=(1024, 768), validator=iterable_of(tuple, int), convert=tuple)
+    window_hint_context_version_major = attr.ib(default=3, validator=instance_of(int))
+    window_hint_context_version_minor = attr.ib(default=3, validator=instance_of(int))
+    window_hint_opengl_forward_compat = attr.ib(default=True, validator=instance_of(bool))
+    window_hint_opengl_profile = attr.ib(default=glfw.OPENGL_CORE_PROFILE, validator=instance_of(int))
+    swap_interval = attr.ib(default=1, validator=instance_of(int))
+    clear_color = attr.ib(default=(0, 0, 0, 1), validator=iterable_of(tuple, int), convert=tuple)
+    clear_bits = attr.ib(default=(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT), validator=instance_of(int))
+    field_of_view = attr.ib(default=math.pi, validator=instance_of(float))
+    near_plane = attr.ib(default=0.1, validator=instance_of(float))
+    far_plane = attr.ib(default=100.0, validator=instance_of(float))
+
+    @property
+    def window_hints(self):
+        """
+        Return the GLFW window hints as dictionary.
+
+        :return:
+        """
+        window_hints = dict()
+        for k, v in attr.asdict(self, filter=lambda a, v: a.name.startswith("window_hint")).items():
+            name = getattr(glfw, k.replace("window_hint_", "").upper())
+            window_hints[name] = v
+
+        return window_hints
+
+    @classmethod
+    def from_dict(cls, **config):
+        """
+        Create an instance from a dictionary.
+
+        :param config:
+        :return:
+        """
+        modified_attributes = {a.name: config[a.name] for a in attr.fields(cls) if a.name in config}
+        return cls(**modified_attributes)
+
+
+@attr.s
+class KeyMap(object):
+    """
+    KeyMap shall hold all known keys and corresponding actions.
+    """
+    class Key(enum.Enum):
+        A = glfw.KEY_A
+        D = glfw.KEY_D
+        S = glfw.KEY_S
+        W = glfw.KEY_W
+        X = glfw.KEY_X
+        Z = glfw.KEY_Z
+
+        @classmethod
+        def coerce(cls, value):
+            if isinstance(value, cls):
+                return value
+            elif isinstance(value, str):
+                try:
+                    return cls[value]
+                except KeyError:
+                    raise ValueError("Key {} is not known.".format(value))
+            else:
+                raise ValueError("Key {} is not known.".format(value))
+
+    _left = attr.ib(validator=instance_of(Key), convert=Key.coerce)
+    _right = attr.ib(validator=instance_of(Key), convert=Key.coerce)
+    _up = attr.ib(validator=instance_of(Key), convert=Key.coerce)
+    _down = attr.ib(validator=instance_of(Key), convert=Key.coerce)
+    _forward = attr.ib(validator=instance_of(Key), convert=Key.coerce)
+    _backward = attr.ib(validator=instance_of(Key), convert=Key.coerce)
+
+    @property
+    def left(self):
+        return self._left.value
+
+    @property
+    def right(self):
+        return self._right.value
+
+    @property
+    def up(self):
+        return self._up.value
+
+    @property
+    def down(self):
+        return self._down.value
+
+    @property
+    def forward(self):
+        return self._forward.value
+
+    @property
+    def backward(self):
+        return self._backward.value
+
+    @classmethod
+    def from_dict(cls, **config):
+        """
+        Create an instance from a dictionary.
+
+        :param config:
+        :return:
+        """
+        return cls(**config)
+
+    def __iter__(self):
+        """
+        Allow iteration over the KeyMap member variables.
+
+        :return:
+        """
+        for member in attr.astuple(self):
+            yield member.value
