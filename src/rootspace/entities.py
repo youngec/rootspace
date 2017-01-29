6e8c2df8565eceeeffd8713765756e435825b8e2
Eleanore Young <youngelliecaitlin@gmail.com>
2017-01-29 09:09:20 +0100

    Got rid of TestEntity, but added StaticObject and PhysicalObject.

--- src/rootspace/entities.py
+++ src/rootspace/entities.py
@@ -39,16 +39,22 @@ class Entity(object, metaclass=EntityMeta):
 
 
 @attr.s(hash=False)
-class TestEntity(Entity):
-    transform = attr.ib(validator=instance_of(Transform))
-    physics_properties = attr.ib(validator=instance_of(PhysicsProperties))
-    physics_state = attr.ib(validator=instance_of(PhysicsState))
-    model = attr.ib(validator=instance_of(Model))
-
-
-@attr.s(hash=False)
 class Camera(Entity):
     transform = attr.ib(validator=instance_of(Transform))
     physics_properties = attr.ib(validator=instance_of(PhysicsProperties))
     physics_state = attr.ib(validator=instance_of(PhysicsState))
     projection = attr.ib(validator=instance_of(Projection))
+
+
+@attr.s(hash=False)
+class StaticObject(Entity):
+    transform = attr.ib(validator=instance_of(Transform))
+    model = attr.ib(validator=instance_of(Model))
+
+
+@attr.s(hash=False)
+class PhysicalObject(Entity):
+    transform = attr.ib(validator=instance_of(Transform))
+    model = attr.ib(validator=instance_of(Model))
+    physics_properties = attr.ib(validator=instance_of(PhysicsProperties))
+    physics_state = attr.ib(validator=instance_of(PhysicsState))
