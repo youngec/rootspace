3ca5cb6304e467ab0b45692f317500545385304f
Eleanore Young <youngelliecaitlin@gmail.com>
2017-03-05 14:40:26 +0100

    Re-added the debug-specific cursor mod setting, since mouse movement now works well.

--- src/rootspace/core.py
+++ src/rootspace/core.py
@@ -475,8 +475,9 @@ class World(object):
         :return:
         """
         # Set the cursor behavior
-        glfw.set_input_mode(self.ctx.window, glfw.CURSOR, new_scene.cursor_mode)
-        glfw.set_cursor_pos(self.ctx.window, *new_scene.cursor_origin)
+        if not self.ctx.debug:
+            glfw.set_input_mode(self.ctx.window, glfw.CURSOR, new_scene.cursor_mode)
+            glfw.set_cursor_pos(self.ctx.window, *new_scene.cursor_origin)
 
         # Enable the OpenGL depth buffer
         if new_scene.enable_depth_test:
