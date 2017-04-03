dbe4e30dea70bd6b704f91e15e6d5535fd9e70b5
Eleanore Young <youngelliecaitlin@gmail.com>
2017-04-03 14:53:14 +0200

    Updated the versioneer install

--- src/rootspace/__init__.py
+++ src/rootspace/__init__.py
@@ -23,3 +23,7 @@ elif sys.platform == "win32":
 
 # Configure PyOpenGL
 # OpenGL.ERROR_ON_COPY = True
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
