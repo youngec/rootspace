d82e09086af63423f4dc694cdc2d14e01936e1bf
Eleanore Young <youngelliecaitlin@gmail.com>
2017-05-07 11:37:43 +0200

    Added unit tests for utilities functions and removed dead code.

--- src/rootspace/tests/test_utilities.py
+++ src/rootspace/tests/test_utilities.py
@@ -1,54 +1,52 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import logging
+import weakref
+import uuid
 
-from rootspace.utilities import as_range, slice_length, linearize_scalar_indices, normalize_slice, linearize_indices, get_sub_shape
-
-def test_normalize_slice():
-    assert normalize_slice(slice(None), 100) == slice(0, 100, 1)
-
-
-def test_as_range():
-    assert as_range(slice(None), 100) == range(100)
+from rootspace.utilities import underscore_to_camelcase, \
+    camelcase_to_underscore, get_log_level, configure_logger, \
+    Loggers, to_ref, to_uuid
 
 
-def test_slice_length(mocker):
-    assert slice_length(slice(None), 100) == 100
-
-    mock_as_range = mocker.patch("rootspace.utilities.as_range")
-    slice_length(slice(1, 5, 1), 6)
-    mock_as_range.assert_called_once_with(slice(1, 5, 1), 6)
-
-
-def test_get_sub_shape():
-    shape = (4, 4)
-    # assert get_sub_shape(shape, (4,)) == (1, 1)
-    assert get_sub_shape(shape, (2, 2)) == (1, 1)
-    assert get_sub_shape(shape, (2, (0, 1))) == (1, 2)
-    assert get_sub_shape(shape, ((0, 1), 2)) == (2, 1)
-    assert get_sub_shape(shape, ((0, 1), (0, 1))) == (2, 2)
-    assert get_sub_shape(shape, (2, slice(3))) == (1, 3)
-    assert get_sub_shape(shape, (slice(3), 2)) == (3, 1)
-    assert get_sub_shape(shape, (slice(3), slice(3))) == (3, 3)
-    assert get_sub_shape(shape, (slice(3), (0, 1))) == (3, 2)
-    assert get_sub_shape(shape, ((0, 1), slice(3))) == (2, 3)
-    with pytest.raises(ValueError):
-        get_sub_shape(shape, (None, ))
-    with pytest.raises(TypeError):
-        get_sub_shape(shape, (1.0, 1.0))
+@pytest.mark.parametrize(("underscore", "camelcase"), (
+    ("some_complicated_name", "SomeComplicatedName"),
+    ("some_name", "SomeName"),
+    ("some", "Some"),
+))
+def test_case_conversion(underscore, camelcase):
+    assert underscore_to_camelcase(underscore) == camelcase
+    assert camelcase_to_underscore(camelcase) == underscore
 
 
-def test_linearize_scalar_indices():
-    assert linearize_scalar_indices((2, 4), 1, 3) == 7
+def test_to_ref():
+    class SomeClass(object):
+        pass
+
+    value = SomeClass()
+    wr = weakref.ref(value)
+    assert to_ref(wr) is wr
+    assert to_ref(None) is None
+    assert isinstance(to_ref(value), weakref.ReferenceType)
 
 
-def test_linearize_indices():
-    assert linearize_indices((2, 4), (1, 3)) == (7, )
-    assert linearize_indices((2, 4), (1, (0, 1))) == (4, 5)
-    assert linearize_indices((2, 4), ((0, 1), 1)) == (1, 5)
-    assert linearize_indices((2, 4), ((0, 1), (0, 1, 2))) == (0, 1, 2, 4, 5, 6)
-    assert linearize_indices((2, 4), (1, slice(3))) == (4, 5, 6)
-    assert linearize_indices((2, 4), (slice(2), 1)) == (1, 5)
-    assert linearize_indices((2, 4), (slice(2), slice(3))) == (0, 1, 2, 4, 5, 6)
-    assert linearize_indices((2, 4), (slice(2), (0, 1))) == (0, 1, 4, 5)
-    assert linearize_indices((2, 4), ((0, 1), slice(3))) == (0, 1, 2, 4, 5, 6)
+def test_to_uuid():
+    value = uuid.uuid4()
+    assert to_uuid(value) is value
+    assert to_uuid(str(value)) == value
+    assert to_uuid(None) is None
+
+
+@pytest.mark.parametrize(("verbosity", "expected"), (
+    (0, logging.ERROR),
+    (1, logging.WARN),
+    (2, logging.INFO),
+    (3, logging.DEBUG)
+))
+def test_get_log_level(verbosity, expected):
+    assert get_log_level(verbosity) == expected
+
+
+def test_configure_logger():
+    assert isinstance(configure_logger("test", logging.ERROR), Loggers)
--- src/rootspace/utilities.py
+++ src/rootspace/utilities.py
@@ -7,125 +7,13 @@ import re
 import uuid
 import warnings
 import weakref
-import itertools
-from typing import Tuple, Optional, Any, Union
+from typing import Optional, Any, Union
 
 import attr
 
-__docformat__ = "restructuredtext"
 FIRST_CAP_RE = re.compile(r"(.)([A-Z][a-z]+)")
 ALL_CAP_RE = re.compile(r"([a-z0-9])([A-Z])")
-
-
-def normalize_slice(s: slice, sequence_length: int) -> slice:
-    """
-    Normalize a given slice. That is, convert a slice with
-    None attributes to contain the specified bounds:
-
-    slice(None, None, None) becomes
-    slice(0, sequence_length, 1)
-
-    :param s:
-    :param sequence_length:
-    :return:
-    """
-    return slice(*s.indices(sequence_length))
-
-
-def as_range(s: slice, sequence_length: int) -> range:
-    """
-    Convert a slice to a range.
-
-    slice(None, None, None) becomes
-    range(0, sequence_length, 1)
-
-    :param s:
-    :param sequence_length:
-    :return:
-    """
-    return range(*s.indices(sequence_length))
-
-
-def slice_length(s: slice, sequence_length: int) -> int:
-    """
-    Return the length of the sliced portion.
-
-    :param s:
-    :param sequence_length:
-    :return:
-    """
-    return len(tuple(as_range(s, sequence_length)))
-
-
-def get_sub_shape(shape: Tuple[int, ...], indices: Tuple[Union[int, slice, Tuple[int, ...]], ...]) -> Tuple[int, ...]:
-    """
-    For a given set of multi-dimensional indices,
-    return the shape of the resulting sub-matrix.
-
-    :param shape:
-    :param indices:
-    :return:
-    """
-    sub_shape = list()
-    for i, k in enumerate(indices):
-        if isinstance(k, int):
-            sub_shape.append(1)
-        elif isinstance(k, tuple):
-            sub_shape.append(len(k))
-        elif isinstance(k, slice):
-            sub_shape.append(slice_length(k, shape[i]))
-        else:
-            raise TypeError("Expected the tuple indices to be either int, tuple, or slice.")
-
-    if len(sub_shape) == 1:
-        sub_shape.append(1)
-
-    return tuple(sub_shape)
-
-
-def linearize_scalar_indices(shape: Tuple[int, ...], *idx: Tuple[int, ...]) -> int:
-    """
-    From a given set of multi-dimensional indices, construct
-    the corresponding linear index.
-
-    :param shape:
-    :param idx:
-    :return:
-    """
-    return sum(i * s for i, s in zip((0, ) + idx, shape + (1, )))
-
-
-def linearize_indices(shape: Tuple[int, ...], indices: Tuple[Union[int, slice, Tuple[int, ...]], ...]) -> Tuple[int, ...]:
-    """
-    For given multi-dimensional indices, provide a linear index. This also works for sliced and tuple indices.
-
-    :param shape:
-    :param i:
-    :param j:
-    :return:
-    """
-    i, j = indices
-
-    if isinstance(i, int) and isinstance(j, int):  # Single 2-index
-        return (linearize_scalar_indices(shape, i, j), )
-    elif isinstance(i, int) and isinstance(j, tuple):
-        return tuple(linearize_scalar_indices(shape, i, e) for e in j)
-    elif isinstance(i, tuple) and isinstance(j, int):
-        return tuple(linearize_scalar_indices(shape, e, j) for e in i)
-    elif isinstance(i, tuple) and isinstance(j, tuple):
-        return tuple(linearize_scalar_indices(shape, e, f) for e, f in itertools.product(i, j))
-    elif isinstance(i, int) and isinstance(j, slice):  # Partial sliced 2-index
-        return tuple(linearize_scalar_indices(shape, i, e) for e in as_range(j, shape[1]))
-    elif isinstance(i, slice) and isinstance(j, int):  # Partial sliced 2-index
-        return tuple(linearize_scalar_indices(shape, e, j) for e in as_range(i, shape[0]))
-    elif isinstance(i, slice) and isinstance(j, slice):  # Full sliced 2-index
-        return tuple(linearize_scalar_indices(shape, e, f) for e, f in itertools.product(as_range(i, shape[0]), as_range(j, shape[1])))
-    elif isinstance(i, slice) and isinstance(j, tuple):
-        return tuple(linearize_scalar_indices(shape, e, f) for e, f in itertools.product(as_range(i, shape[0]), j))
-    elif isinstance(i, tuple) and isinstance(j, slice):
-        return tuple(linearize_scalar_indices(shape, e, f) for e, f in itertools.product(i, as_range(j, shape[1])))
-    else:
-        raise TypeError("Expected the tuple indices to be either int or slice, not '{}' and '{}'.".format(type(i), type(j)))
+Loggers = collections.namedtuple("Loggers", ("project", "py_warnings"))
 
 
 def underscore_to_camelcase(name: str) -> str:
@@ -157,17 +45,15 @@ def to_ref(value: Any) -> Optional[weakref.ReferenceType]:
     :param value:
     :return:
     """
-    if value is not None:
-        return weakref.ref(value)
-    elif isinstance(value, weakref.ReferenceType):
+    if isinstance(value, weakref.ReferenceType):
         return value
     elif value is None:
         return None
     else:
-        raise TypeError("Expected 'value' to be either an object, a ReferenceType or None.")
+        return weakref.ref(value)
 
 
-def to_uuid(value: Union[int, bytes, str, uuid.UUID]) -> Optional[uuid.UUID]:
+def to_uuid(value: Optional[Union[str, uuid.UUID]]) -> Optional[uuid.UUID]:
     """
     Convert the input to a UUID value. This function is idempotent and passes None unmodified.
 
@@ -176,7 +62,7 @@ def to_uuid(value: Union[int, bytes, str, uuid.UUID]) -> Optional[uuid.UUID]:
     """
     if isinstance(value, uuid.UUID):
         return value
-    elif isinstance(value, (str, bytes, int)):
+    elif isinstance(value, str):
         return uuid.UUID(value)
     elif value is None:
         return None
@@ -184,20 +70,17 @@ def to_uuid(value: Union[int, bytes, str, uuid.UUID]) -> Optional[uuid.UUID]:
         raise TypeError("Expected 'value' to be either a UUID, a string or None.")
 
 
-def get_log_level(verbose: int) -> int:
+def get_log_level(verbosity: int) -> int:
     """
     Determine the logging level from the verbose and debug flags.
-
-    :param verbose:
-    :return:
     """
-    if verbose <= 0:
+    if verbosity <= 0:
         log_level = logging.ERROR
-    elif verbose == 1:
+    elif verbosity == 1:
         log_level = logging.WARN
-    elif verbose == 2:
+    elif verbosity == 2:
         log_level = logging.INFO
-    elif verbose >= 3:
+    elif verbosity >= 3:
         log_level = logging.DEBUG
     else:
         raise ValueError("Only four verbosity levels are understood: 0, 1, 2 and 3.")
@@ -205,7 +88,7 @@ def get_log_level(verbose: int) -> int:
     return log_level
 
 
-def configure_logger(name: str, log_level: int, log_path: Optional[str] = None, with_warnings: Optional[bool] = True):
+def configure_logger(name: str, log_level: int, log_path: Optional[str] = None, with_warnings: Optional[bool] = True) -> Loggers:
     """
     Configure the project logger of the specified name
     using colorlog.
@@ -248,8 +131,7 @@ def configure_logger(name: str, log_level: int, log_path: Optional[str] = None, 
         py_warnings.setLevel(log_level)
         py_warnings.addHandler(default_handler)
 
-    loggers = collections.namedtuple("loggers", ("project", "py_warnings"))
-    return loggers(project_logger, py_warnings)
+    return Loggers(project_logger, py_warnings)
 
 
 @attr.s(repr=False, slots=True)
