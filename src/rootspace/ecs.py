8ae7ae617514da30a73d63f7ce91cec718960080
Eleanore Young <youngelliecaitlin@gmail.com>
2017-05-31 12:51:29 +0200

    Added a re-implementation of the entity-component-system

---
+++ src/rootspace/ecs.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+
+import abc
+from typing import List, TypeVar, Generic, Optional, Sequence, Type, Deque, Iterator
+from collections import deque
+
+
+C = TypeVar("C")
+
+
+class Entity(object):
+    __slots__ = ("name", "uuid", "idx")
+
+    def __init__(self, name: str, uuid: int, idx: int) -> None:
+        self.name = name
+        self.uuid = uuid
+        self.idx = idx
+
+    def increment(self) -> None:
+        self.uuid += 1
+        self.idx += 1
+
+    def clone(self) -> "Entity":
+        return Entity(
+            name=self.name,
+            uuid=self.uuid, 
+            idx=self.idx
+        )
+
+    def __repr__(self) -> str:
+        return "Entity({})".format(self.name)
+    
+    def __str__(self) -> str:
+        return repr(self)
+
+
+class EntityDataIndex(object):
+    __slots__ = ("uuid", "data_idx")
+
+    def __init__(self, uuid: int, data_idx: int) -> None:
+        self.uuid = uuid
+        self.data_idx = data_idx
+
+    @classmethod
+    def new(cls) -> "EntityDataIndex":
+        return cls(
+            uuid=0,
+            data_idx=0
+        )
+
+    def clone(self) -> "EntityDataIndex":
+        return EntityDataIndex(
+            uuid=self.uuid,
+            data_idx=self.data_idx
+        )
+
+
+class ComponentContainer(Generic[C]):
+    __slots__ = ("data", "entities", "indices")
+
+    def __init__(self, data: List[C], entities: List[Entity], indices: List[EntityDataIndex]) -> None:
+        self.data = data
+        self.entities = entities
+        self.indices = indices
+
+    @classmethod
+    def new(cls) -> "ComponentContainer":
+        return cls(
+            data=list(),
+            entities=list(),
+            indices=list()
+        )
+
+    def contains(self, entity: Entity) -> bool:
+        assert(entity.uuid > 0)
+        return (entity.idx < len(self.indices)) and (self.indices[entity.idx].uuid == entity.uuid)
+
+    def add(self, entity: Entity, component: C) -> None:
+        assert(len(self.data) == len(self.entities))
+        if self.contains(entity):
+            self.data[self.indices[entity.idx].data_idx] = component
+        else:
+            if entity.idx >= len(self.indices):
+                # resize the indices vector
+                for _ in range(len(self.indices), entity.idx + 1):
+                    self.indices.append(EntityDataIndex.new())
+            
+            self.data.append(component)
+            self.entities.append(entity.clone())
+            self.indices[entity.idx] = EntityDataIndex(
+                uuid=entity.uuid,
+                data_idx=len(self.data) - 1
+            )
+
+    def remove(self, entity: Entity) -> None:
+        assert(len(self.data) == len(self.entities))
+        if self.contains(entity):
+            removed_idx = self.indices[entity.idx].clone()
+            self.indices[entity.idx] = EntityDataIndex.new()
+
+            if removed_idx.data_idx != len(self.entities) - 1:
+                last_entity = self.entities[-1].clone()
+                self.entities.pop(removed_idx.data_idx)
+                self.indices[last_entity.idx] = EntityDataIndex(
+                    uuid=last_entity.uuid,
+                    data_idx=removed_idx.data_idx
+                )
+            else:
+                self.entities.pop(removed_idx.data_idx)
+
+    def get(self, entity: Entity) -> Optional[C]:
+        if self.contains(entity):
+            return self.data[self.indices[entity.idx].data_idx]
+        else:
+            return None
+
+    def iter_ent(self) -> Iterator[Entity]:
+        yield from self.entities
+
+    def __iter__(self) -> Iterator[C]:
+        yield from self.data
+
+
+class ViewTrait(object, metaclass=abc.ABCMeta):
+    __slots__ = ()
+
+    @abc.abstractmethod
+    def fetch(self) -> "ViewTrait":
+        pass
+
+
+class AssemblyTrait(object, metaclass=abc.ABCMeta):
+    __slots__ = ()
+
+    @classmethod
+    @abc.abstractmethod
+    def new(cls) -> "AssemblyTrait":
+        pass
+
+    @abc.abstractmethod
+    def match_mask(self, entity: Entity, mask: int) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def remove(self, entity: Entity) -> None:
+        pass
+
+    @abc.abstractmethod
+    def get_view(self, entity: Entity) -> ViewTrait:
+        pass
+
+
+class SystemTrait(object, metaclass=abc.ABCMeta):
+    __slots__ = ()
+
+    @abc.abstractmethod
+    def get_name(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def get_mask(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def run(self, components: Sequence[ViewTrait]) -> None:
+        pass
+
+
+class World(object):
+    __slots__ = ("next_entity", "free_indices", "entities", "components", "systems")
+
+    def __init__(self, next_entity: Entity, free_indices: Deque[int], entities: ComponentContainer[bool], components: AssemblyTrait, systems: List[SystemTrait]) -> None:
+        self.next_entity = next_entity
+        self.free_indices = free_indices
+        self.entities = entities
+        self.components = components
+        self.systems = systems
+
+    @classmethod
+    def new(cls, assembly: Type[AssemblyTrait]) -> "World":
+        return cls(
+            next_entity=Entity(name="", uuid=1, idx=0),
+            free_indices=deque(),
+            entities=ComponentContainer.new(),
+            components=assembly.new(),
+            systems=list()
+        )
+
+    def make(self, name: str) -> Entity:
+        entity = Entity(
+            name=name,
+            uuid=self.next_entity.uuid,
+            idx=(self.free_indices.popleft() if len(self.free_indices) > 0 else self.next_entity.idx)
+        )
+        self.next_entity.increment()
+        self.entities.add(entity, True)
+        return entity
+
+    def remove(self, entity: Entity) -> None:
+        self.free_indices.append(entity.idx)
+        self.entities.remove(entity)
+        self.components.remove(entity)
+
+    def add_system(self, system: SystemTrait) -> None:
+        self.systems.append(system)
+
+    def run(self) -> None:
+        assembly = self.components
+
+        for system in self.systems:
+            mask = system.get_mask()
+            candidates = (e.clone() for e in self.entities.iter_ent() if assembly.match_mask(e, mask))
+            components = [assembly.get_view(e) for e in candidates]
+
+            if len(components) > 0:
+                system.run(components)
---
+++ src/rootspace/tests/test_ecs.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from typing import Sequence
+
+import pytest
+
+from rootspace.ecs import Entity, ComponentContainer, ViewTrait, AssemblyTrait, SystemTrait, World
+
+
+class ExampleView(ViewTrait):
+    __slots__ = ("a", "b")
+
+    def __init__(self, a: int, b: float) -> None:
+        self.a = a
+        self.b = b
+
+    def fetch(self) -> "ExampleView":
+        return self
+
+
+class ExampleAssembly(AssemblyTrait):
+    __slots__ = ("a", "b")
+
+    def __init__(self, a: ComponentContainer[int], b: ComponentContainer[float]) -> None:
+        self.a = a
+        self.b = b
+
+    @classmethod
+    def new(cls) -> "ExampleAssembly":
+        return cls(
+            a=ComponentContainer.new(),
+            b=ComponentContainer.new()
+        )
+
+    def match_mask(self, entity: Entity, mask: int) -> bool:
+        a = (mask & 0b01) > 0
+        b = (mask & 0b10) > 0
+        c = self.a.contains(entity)
+        d = self.b.contains(entity)
+        return (a and c) or (b and d) or ((a or b) and c and d)
+
+    def remove(self, entity: Entity) -> None:
+        self.a.remove(entity)
+        self.b.remove(entity)
+
+    def get_view(self, entity: Entity) -> ViewTrait:
+        return ExampleView(
+            a=self.a.get(entity),
+            b=self.b.get(entity)
+        )
+
+
+class ExampleSystem(SystemTrait):
+    __slots__ = ()
+
+    def get_name(self) -> str:
+        return "ExampleSystem"
+
+    def get_mask(self) -> int:
+        return 0b01
+
+    def run(self, components: Sequence[ViewTrait]) -> None:
+        print(components)
+        assert len(components) == 2
+        assert all([c.a is not None for c in components])
+
+
+class TestWorld(object):
+    def test_world(self) -> None:
+        world = World.new(ExampleAssembly)
+        e = world.make("e")
+        world.components.a.add(e, 1)
+        world.components.b.add(e, 1.0)
+
+        f = world.make("f")
+        world.components.a.add(f, 2)
+
+        g = world.make("g")
+        world.components.b.add(g, 3.0)
+
+        world.add_system(ExampleSystem())
+        world.run()
